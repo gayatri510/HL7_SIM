4ee90e30af71be78f8f944a159a199a6ad6424ff
Gayatri NARUMANCHI <gnaruman@capsuletech.com>
2018-10-25 12:28:10 -0400

    Added few default vvalues

--- config.py
+++ config.py
@@ -18,27 +18,29 @@ configurationbox_segments = OrderedDict([('MSH Field Count', '17'),
                                          ('OBX Subcomponent Count', '5'),                                        
                                          ])
 
-header_variables = OrderedDict([('MSH-3',   '6510'),
-                                ('MSH-3-1', '7914'),
-                                ('MSH-3-2', '1911'),
-                                ('MSH-4',   '6511'),
-                                ('MSH-5',   '6512'),
-                                ('MSH-6',   '6513'),                                      
-                                ('MSH-7',   '2255'),
-                                ('PID-3',   '9569'),
-                                ('PID-3-1', '1929'),                                        
-                                ('PID-5',   '1930'),
-                                ('PID-5-1', '8340'),
-                                ('PID-5-2', '2901'),
-                                ('PID-7',   '1220'),
-                                ('PID-8',   '170'),
-                                ('PID-18',  '8036'),   
-                                ('PV1-2',   '6521'),
-                                ('OBR-2',   '9593'),
-                                ('OBR-3',   '8102')                                   
+
+HEADER_VARIABLES = OrderedDict([('6510', 'MSH-3'),
+                                ('7914', 'MSH-3-1'),
+                                ('1911', 'MSH-3-2'),
+                                ('6511', 'MSH-4'),
+                                ('6512', 'MSH-5'),
+                                ('6513', 'MSH-6'),                                      
+                                ('2255', 'MSH-7'),
+                                ('9569', 'PID-3'),
+                                ('1929', 'PID-3-1'),                                        
+                                ('1930', 'PID-5'),
+                                ('8340', 'PID-5-1'),
+                                ('2901', 'PID-5-2'),
+                                ('1220', 'PID-7'),
+                                ('170',  'PID-8'),
+                                ('8036', 'PID-18'),   
+                                ('6521', 'PV1-2'),
+                                ('9593', 'OBR-2'),
+                                ('8102', 'OBR-3')                                   
                                 ])
 
 
 default_hl7_segments = "MSH, PID, PV1, OBR"
+DEFAULT_CALCULATED_VARIABLES = "1190, 2583, 5815, 5816"
 obr_7_timestamp_default_state = True
 obx_14_timestamp_default_state = True
